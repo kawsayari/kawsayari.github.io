b9cd4e5d1d4ebb85bd9c7186f3d444ecf6a3d10c
adrianfcamposvelit-ai <adrianfcamposvelit@gmail.com>
2026-06-24 16:07:35 -0500

    Conexión del mural público

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.KAWSAYARI_CONFIG = {
   siteUrl: "https://kawsayari.github.io/",
-  formEmbedUrl: "",
-  muralCsvUrl: "",
+  formEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLSfdRZMfVcTG1AIa4dtl4wiLcPNiwxoQtzs5L1-rNHzjG_pVBQ/viewform?usp=publish-editor",
+  muralCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5H5dxEXg3PA7Zr53-wUeP0HuazE4hJZfI51KSAhmThOtI41BM1RYToTYTEePtKiSmD3_L1k_CQUL1/pub?gid=1515463743&single=true&output=csv",
   contactEmail: "",
   instagramHandle: "",
   whatsappChannel: "",
