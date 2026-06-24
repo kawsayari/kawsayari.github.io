af2f0adb882937d166909f6bb530824ea9299277
adrianfcamposvelit-ai <adrianfcamposvelit@gmail.com>
2026-06-24 15:25:30 -0500

    Kawsayari Viral Engine

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+window.KAWSAYARI_CONFIG = {
+  siteUrl: "https://kawsayari.github.io/",
+  formEmbedUrl: "",
+  muralCsvUrl: "",
+  contactEmail: "",
+  instagramHandle: "",
+  whatsappChannel: "",
+  tiktokHandle: "",
+  facebookPage: ""
+};
